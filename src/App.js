670d9c21d12f88c944e58251491eb47602ebfe20
vinay kumar <vinnuborra3@gmaie.com>
2023-06-07 20:09:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,20 @@ import Vote from './components/Vote';
 
 const App = () => {
   return (
-      <div className="flex flex-col items-center justify-center h-screen bg-cyan-950 ">
-      <div className="container mx-auto p-8 bg-orange-100 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-4 text-center">Voting Platform</h1>
+     <div className="flex flex-col items-center justify-center h-screen bg-cyan-950">
+  <div className="container mx-auto p-8 bg-orange-100 rounded-lg shadow-lg flex flex-col items-center">
+    <h1 className="text-3xl font-bold mb-4 text-center">Voting Platform</h1>
 
-      <ConnectWalletButton />
- 
-      <CreateTopic />
+    <ConnectWalletButton />
 
-      <CreateOption />
+    <CreateTopic />
 
-      <Vote /> 
-    </div>
-    </div>
+    <CreateOption />
+
+    <Vote />
+  </div>
+</div>
+
   )
 
 };
